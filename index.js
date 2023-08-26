23edb68e830da25be4ee9ef7346d415cdd3f7b13
Jami <jamsheduddin03@gmail.com>
2023-08-26 21:29:30 +0600

    'setup'

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((error) => console.log(error));
+
+app.get("/", (req, res) => {
+  res.send("mongoose crud is running");
+});
+
+// error handler
+
+app.listen(process.env.PORT, () => {
+  console.log(`mongoose_crud running on port ${process.env.PORT}`);
+});
