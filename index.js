a60188e5b94d35e728664ff870a97c75c9eb4350
Jami <jamsheduddin03@gmail.com>
2023-08-26 23:48:54 +0600

    todo create

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const todoHandler = require("./routeHandler/todoHandler");
 const cors = require("cors");
 const app = express();
 
@@ -14,6 +15,8 @@ mongoose
     console.log("connected to database");
   })
   .catch((error) => console.log(error));
+
+app.use("/todo", todoHandler);
 
 app.get("/", (req, res) => {
   res.send("mongoose crud is running");
---
+++ routeHandler/todoHandler.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const todoSchema = require("../schema/todoSchema");
+
+const Todo = new mongoose.model("Todo", todoSchema);
+//get all todo
+router.get("/", async (req, res) => {});
+
+//get a todo by id
+router.get("/:id", async (req, res) => {});
+
+//post a todo
+router.post("/", async (req, res) => {
+  const newTodo = new Todo(req.body);
+  await newTodo
+    .save()
+    .then(() => {
+      res.status(200).json({ message: "Todo inserted successfully" });
+    })
+    .catch((error) => res.status(401).json({ error: "Something went wrong" }));
+});
+
+//post multiple todo
+router.post("/multiple", async (req, res) => {});
+
+//  update todo
+router.put("/:id", async (req, res) => {});
+
+//  delete todo
+router.delete("/:id", async (req, res) => {});
+
+module.exports = router;
